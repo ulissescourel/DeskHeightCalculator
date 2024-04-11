348688343bde60060fd7fd59b727ed1e7f0738f9
Ulisses Courel <ulisses.courel@gmail.com>
2024-04-11 10:38:21 -0400

    handling decimals on example 2

--- Calculate2.py
+++ Calculate2.py
@@ -1,10 +1,14 @@
-def desk_chair_height(height):
+from math import ceil
+
+def desk_chair_height(height, decimal_pref):
     feet, inches = height.split('\'')
-    total_inches = int(feet) * 12 + int(inches[:-1])
-    desk_height = total_inches * 0.56  # 56% of total height
-    chair_height = total_inches * 0.29  # 29% of total height
+    total_inches = int(feet) * 12 + int(inches)
+    multiplier = 10**(decimal_pref)  # convert decimal_pref to multiplier
+    desk_height = round(total_inches * 0.56 * multiplier) / multiplier  # 56% of total height, rounded to nearest tenth
+    chair_height = round(total_inches * 0.29 * multiplier) / multiplier  # 29% of total height, rounded to nearest tenth
     return {'desk_height': desk_height, 'chair_height': chair_height}
 
 height = "5'10\""  # 5 feet 10 inches
-suggested_heights = desk_chair_height(height)
-print(suggested_heights)
+desk_chair_height(height, 1)  # 1 decimal places
+desk_chair_height(height, 2)  # 2 decimal places
+desk_chair_height(height, 0)  # no decimals
